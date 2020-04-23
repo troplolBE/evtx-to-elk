d3e0b922addd64e2ae51124449fdcee3403a7ecf
troplolBE <thomascastronovo@icloud.com>
2020-04-23 09:56:48 +0200

    Add project files.

---
+++ evtx-to-elk/Form1.Designer.vb
@@ -0,0 +1,199 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.lblStatus = New System.Windows.Forms.Label()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.FolderBrowserDialog2 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(13, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(104, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Winlogbeat directory"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(16, 30)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(236, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(259, 28)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(85, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Choose folder"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(259, 86)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(85, 23)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "Choose folder"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(16, 88)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(236, 20)
+        Me.TextBox2.TabIndex = 4
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(13, 71)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(73, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Logs directory"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(259, 143)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(85, 23)
+        Me.Button3.TabIndex = 8
+        Me.Button3.Text = "Choose folder"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(16, 145)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(236, 20)
+        Me.TextBox3.TabIndex = 7
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(13, 128)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(90, 13)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Custom config file"
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(122, 199)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(193, 23)
+        Me.Button4.TabIndex = 9
+        Me.Button4.Text = "Import log files"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(1, 271)
+        Me.ProgressBar1.Maximum = 1000
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(438, 23)
+        Me.ProgressBar1.TabIndex = 10
+        '
+        'lblStatus
+        '
+        Me.lblStatus.Location = New System.Drawing.Point(1, 246)
+        Me.lblStatus.Name = "lblStatus"
+        Me.lblStatus.Size = New System.Drawing.Size(438, 23)
+        Me.lblStatus.TabIndex = 11
+        Me.lblStatus.Text = "Waiting to import..."
+        Me.lblStatus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'FolderBrowserDialog1
+        '
+        Me.FolderBrowserDialog1.Description = "Select the directory that ontains the winlogbeat executable."
+        Me.FolderBrowserDialog1.RootFolder = System.Environment.SpecialFolder.MyComputer
+        '
+        'FolderBrowserDialog2
+        '
+        Me.FolderBrowserDialog2.Description = "Select file containing all the logs you want to import"
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        Me.OpenFileDialog1.Title = "Select config file"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(438, 320)
+        Me.Controls.Add(Me.lblStatus)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "evtx-to-elk"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button3 As Button
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Button4 As Button
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents lblStatus As Label
+    Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
+    Friend WithEvents FolderBrowserDialog2 As FolderBrowserDialog
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+End Class
---
+++ evtx-to-elk/Form1.vb
@@ -0,0 +1,52 @@
+﻿Public Class Form1
+    Function CheckInputs() As Integer
+        Dim result As Integer = 0
+        If (String.IsNullOrEmpty(TextBox1.Text)) Or Not My.Computer.FileSystem.DirectoryExists(TextBox1.Text) Then
+            result += 1
+        End If
+        If (String.IsNullOrEmpty(TextBox2.Text)) Or Not My.Computer.FileSystem.DirectoryExists(TextBox2.Text) Then
+            result += 1
+        End If
+        If (String.IsNullOrEmpty(TextBox3.Text)) Or Not My.Computer.FileSystem.FileExists(TextBox3.Text) Then
+            result += 1
+        End If
+        Return result
+    End Function
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If (FolderBrowserDialog1.ShowDialog() = DialogResult.OK) Then
+            TextBox1.Text = FolderBrowserDialog1.SelectedPath
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If (FolderBrowserDialog2.ShowDialog() = DialogResult.OK) Then
+            TextBox2.Text = FolderBrowserDialog2.SelectedPath
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If (OpenFileDialog1.ShowDialog() = DialogResult.OK) Then
+            TextBox3.Text = OpenFileDialog1.FileName
+        End If
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        ProgressBar1.Value = 0
+        lblStatus.Text = "Checking inputs..."
+        lblStatus.Update()
+        If CheckInputs() > 0 Then
+            lblStatus.Text = "Input is wrong..."
+            Return
+        End If
+        Dim Logs() As String = IO.Directory.GetFiles(TextBox2.Text)
+        Dim i As Integer = If(Logs.Length > 0, 1000 / Logs.Length, 1000)
+        For Each file In Logs
+            lblStatus.Text = "Sending logfile " & file.Split("\").Last
+            lblStatus.Update()
+            ProgressBar1.Increment(i)
+            Shell(TextBox1.Text & "\winlogbeat.exe -e -c " & TextBox3.Text & " -E EVTX_FILE=" & file,
+                  AppWinStyle.NormalFocus, True)
+        Next
+    End Sub
+End Class
